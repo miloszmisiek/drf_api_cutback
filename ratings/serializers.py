1dd28c6d8f7d2814e1a7e51695d1e5d0e004ebc7
miloszmisiek <84859537+miloszmisiek@users.noreply.github.com>
2022-08-26 13:06:15 +0200

    Add naturaltime display to rating created_at field in RatingSerializer

--- ratings/serializers.py
+++ ratings/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.humanize.templatetags.humanize import naturaltime
 from django.db import IntegrityError
 from rest_framework import serializers
 from .models import Rating
@@ -8,6 +9,7 @@ class RatingSerializer(serializers.ModelSerializer):
     """
     owner = serializers.ReadOnlyField(source='owner.username')
     product_name = serializers.ReadOnlyField(source='product.title')
+    created_at = serializers.SerializerMethodField()
 
     def create(self, validated_data):
         # to avoid crashing server with 500 error
@@ -17,6 +19,9 @@ class RatingSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError({
                 'detail': 'possible duplicate'
             })
+    def get_created_at(self, obj):
+        return naturaltime(obj.created_at)
+
     class Meta:
         model = Rating
         fields = ('id', 'owner', 'product','product_name', 'score', 'created_at',)
