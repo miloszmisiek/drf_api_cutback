b830cfb0b3e36cfaec318bd369007aa8c206abf0
miloszmisiek <84859537+miloszmisiek@users.noreply.github.com>
2022-09-17 13:39:29 +0200

    Refactor delete_default signal in Product model

--- products/models.py
+++ products/models.py
@@ -126,10 +126,18 @@ def delete_default(sender, instance, **kwargs):
     and default image exists as an instance's product key.
     """
     default_image = "../default-image_aqtoyb"
-    ProductImage.objects.filter(
-        product=instance.product.id,
-        image=default_image
-    ).delete() if ProductImage.objects.filter(
-        product=instance.product.id,
-        image=default_image
-    ) else None
+    try:
+        if ProductImage.objects.filter(product=instance.product.id, image=default_image):
+            ProductImage.objects.filter(
+                product=instance.product.id, image=default_image).delete()
+    except:
+        raise APIException("Something went wrong, product instance has no image.")
+
+
+    # ProductImage.objects.filter(
+    #     product=instance.product.id,
+    #     image=default_image
+    # ).delete() if ProductImage.objects.filter(
+    #     product=instance.product.id,
+    #     image=default_image
+    # ) else None
