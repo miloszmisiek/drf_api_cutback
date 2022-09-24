8e42475f9d39a830dc202e7b48195b9ea2ef5a90
miloszmisiek <84859537+miloszmisiek@users.noreply.github.com>
2022-09-24 16:32:05 +0200

    Edit first_name and last_name fields for User model to be blank True

--- profiles/models.py
+++ profiles/models.py
@@ -2,9 +2,12 @@ from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from phonenumber_field.modelfields import PhoneNumberField
+from allauth.account.models import EmailAddress
+from allauth.account.utils import *
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
-from allauth.account.signals import email_confirmed
+from allauth.account.signals import email_confirmed, email_changed, email_added
+
 
 class ProfileRatingManager(models.Manager):
     """
@@ -21,13 +24,14 @@ class ProfileRatingManager(models.Manager):
             all_scores=models.Count('owner__product__product_rating__score')
         )
 
+
 class User(AbstractUser):
     """
     Custom User model inherits from Abstrac User model.
     """
     email = models.EmailField(max_length=50, unique=True, blank=False)
-    first_name = models.CharField(max_length=50, blank=False)
-    last_name = models.CharField(max_length=50, blank=False)
+    first_name = models.CharField(max_length=50, blank=True)
+    last_name = models.CharField(max_length=50, blank=True)
     email_verified = models.BooleanField(default=False)
 
     def __str__(self):
@@ -35,6 +39,11 @@ class User(AbstractUser):
         Returns string representation of owner's username.
         """
         return self.username
+
+    # def add_email_address(self, request, new_email):
+    #     # Add a new email address for the user, and send email confirmation.
+    #     # Old email will remain the primary until the new one is confirmed.
+    #     return EmailAddress.objects.add_email(request, self.user, new_email, confirm=True)
 
 
 class Profile(models.Model):
